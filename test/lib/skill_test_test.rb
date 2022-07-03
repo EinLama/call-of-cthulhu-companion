eb9bf6cda30c67a81b7e2e8a59ee5e2efcab9bbc
Tobias Eilert <github@pentokami.de>
2022-07-03 18:37:14 +0200

    Use assert_equal

--- test/lib/skill_test_test.rb
+++ test/lib/skill_test_test.rb
@@ -3,45 +3,45 @@ require "minitest/mock"
 
 class SkillTestTest < ActiveSupport::TestCase
   test "is a success if rolled number is below or equal skill level" do
-    dice_roller = Object.new
+    dice_roller = mock()
     dice_roller.expects(:roll).with(100).returns(40)
     result = skill_test(50, dice_roller)
-    assert result == :success
+    assert_equal :success, result
 
     dice_roller.expects(:roll).with(100).returns(50)
     result = skill_test(50, dice_roller)
-    assert result == :success
+    assert_equal :success, result
   end
 
   test "is a failure is rolled number is above skill level" do
-    dice_roller = Object.new
+    dice_roller = mock()
     dice_roller.expects(:roll).with(100).returns(51)
 
     result = skill_test(50, dice_roller)
-    assert result == :failure
+    assert_equal :failure, result
   end
 
   test "is a difficult success if rolled number is below or equal 1/2 of skill level" do
-    dice_roller = Object.new
+    dice_roller = mock()
     dice_roller.expects(:roll).with(100).returns(25)
 
     result = skill_test(50, dice_roller)
-    assert result == :difficult_success
+    assert_equal :difficult_success, result
   end
 
   test "is an extreme success if rolled number is below or equal 1/5 of skill level" do
-    dice_roller = Object.new
+    dice_roller = mock()
     dice_roller.expects(:roll).with(100).returns(10)
 
     result = skill_test(50, dice_roller)
-    assert result == :extreme_success
+    assert_equal :extreme_success, result
   end
 
   test "is an critical success if rolled number is 1" do
-    dice_roller = Object.new
+    dice_roller = mock()
     dice_roller.expects(:roll).with(100).returns(1)
 
     result = skill_test(50, dice_roller)
-    assert result == :critical_success
+    assert_equal :critical_success, result
   end
 end

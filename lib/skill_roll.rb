8c5ac31953660f2f18ca83022f6b12fe7219ac31
Tobias Eilert <github@pentokami.de>
2022-07-05 21:52:39 +0200

    SkillRoll class

--- lib/skill_roll.rb
+++ lib/skill_roll.rb
@@ -1,22 +1,37 @@
 
-def skill_roll(level, dice_roller)
-  roll = dice_roller.roll(100)
-
-  result = if roll == 1
-    :critical_success
-  elsif roll <= level / 5
-    :extreme_success
-  elsif roll <= level / 2
-    :hard_success
-  elsif roll <= level
-    :success
-  elsif roll == 100
-    :fumble
-  elsif roll >= 96 && level < 50
-    :fumble
-  else
-    :failure
+class SkillRoll
+  def initialize(dice_roller)
+    @dice_roller = dice_roller
   end
 
-  { rolled: roll, result: result }
+  def skill_roll(level)
+    roll = @dice_roller.roll(100)
+
+    determine_result(level, roll)
+  end
+
+  private
+
+  def determine_result(level, roll)
+    result = if roll == 1
+      :critical_success
+    elsif roll <= level / 5
+      :extreme_success
+    elsif roll <= level / 2
+      :hard_success
+    elsif roll <= level
+      :success
+    elsif roll == 100
+      :fumble
+    elsif roll >= 96 && level < 50
+      :fumble
+    else
+      :failure
+    end
+
+    { rolled: roll, result: result }
+  end
 end
+
+
+
--- test/lib/skill_roll_test.rb
+++ test/lib/skill_roll_test.rb
@@ -8,7 +8,9 @@ class SkillRollTest < ActiveSupport::TestCase
 
   test "is a success if rolled number is below skill level" do
     @dice_roller.expects(:roll).with(100).returns(40)
-    result = skill_roll(50, @dice_roller)
+
+    skill_roll = SkillRoll.new(@dice_roller)
+    result = skill_roll.skill_roll(50)
 
     expected = { result: :success, rolled: 40 }
     assert_equal expected, result
@@ -16,7 +18,9 @@ class SkillRollTest < ActiveSupport::TestCase
 
   test "is a success if rolled number is equal to skill level" do
     @dice_roller.expects(:roll).with(100).returns(50)
-    result = skill_roll(50, @dice_roller)
+
+    skill_roll = SkillRoll.new(@dice_roller)
+    result = skill_roll.skill_roll(50)
 
     expected = { result: :success, rolled: 50 }
     assert_equal expected, result
@@ -25,7 +29,8 @@ class SkillRollTest < ActiveSupport::TestCase
   test "is a failure is rolled number is above skill level" do
     @dice_roller.expects(:roll).with(100).returns(51)
 
-    result = skill_roll(50, @dice_roller)
+    skill_roll = SkillRoll.new(@dice_roller)
+    result = skill_roll.skill_roll(50)
 
     expected = { result: :failure, rolled: 51 }
     assert_equal expected, result
@@ -34,7 +39,8 @@ class SkillRollTest < ActiveSupport::TestCase
   test "is a hard success if rolled number is below or equal 1/2 of skill level" do
     @dice_roller.expects(:roll).with(100).returns(25)
 
-    result = skill_roll(50, @dice_roller)
+    skill_roll = SkillRoll.new(@dice_roller)
+    result = skill_roll.skill_roll(50)
 
     expected = { result: :hard_success, rolled: 25 }
     assert_equal expected, result
@@ -43,7 +49,8 @@ class SkillRollTest < ActiveSupport::TestCase
   test "is an extreme success if rolled number is below or equal 1/5 of skill level" do
     @dice_roller.expects(:roll).with(100).returns(10)
 
-    result = skill_roll(50, @dice_roller)
+    skill_roll = SkillRoll.new(@dice_roller)
+    result = skill_roll.skill_roll(50)
 
     expected = { result: :extreme_success, rolled: 10 }
     assert_equal expected, result
@@ -52,7 +59,8 @@ class SkillRollTest < ActiveSupport::TestCase
   test "is an critical success if rolled number is 1" do
     @dice_roller.expects(:roll).with(100).returns(1)
 
-    result = skill_roll(50, @dice_roller)
+    skill_roll = SkillRoll.new(@dice_roller)
+    result = skill_roll.skill_roll(50)
 
     expected = { result: :critical_success, rolled: 1 }
     assert_equal expected, result
@@ -62,7 +70,8 @@ class SkillRollTest < ActiveSupport::TestCase
     [50, 61, 77, 90].each do |skill_level|
       @dice_roller.expects(:roll).with(100).returns(100)
 
-      result = skill_roll(skill_level, @dice_roller)
+      skill_roll = SkillRoll.new(@dice_roller)
+      result = skill_roll.skill_roll(skill_level)
 
       expected = { result: :fumble, rolled: 100 }
       assert_equal expected, result
@@ -73,7 +82,8 @@ class SkillRollTest < ActiveSupport::TestCase
     [61, 77, 90, 95, 99].each do |rolled_eyes|
       @dice_roller.expects(:roll).with(100).returns(rolled_eyes)
 
-      result = skill_roll(50, @dice_roller)
+      skill_roll = SkillRoll.new(@dice_roller)
+      result = skill_roll.skill_roll(50)
 
       expected = { result: :failure, rolled: rolled_eyes }
       assert_equal expected, result
@@ -84,7 +94,8 @@ class SkillRollTest < ActiveSupport::TestCase
     [96, 97, 98, 99, 100].each do |rolled_eyes|
       @dice_roller.expects(:roll).with(100).returns(rolled_eyes)
 
-      result = skill_roll(49, @dice_roller)
+      skill_roll = SkillRoll.new(@dice_roller)
+      result = skill_roll.skill_roll(49)
 
       expected = { result: :fumble, rolled: rolled_eyes }
       assert_equal expected, result
@@ -95,7 +106,8 @@ class SkillRollTest < ActiveSupport::TestCase
     [95, 90, 60].each do |rolled_eyes|
       @dice_roller.expects(:roll).with(100).returns(rolled_eyes)
 
-      result = skill_roll(49, @dice_roller)
+      skill_roll = SkillRoll.new(@dice_roller)
+      result = skill_roll.skill_roll(49)
 
       expected = { result: :failure, rolled: rolled_eyes }
       assert_equal expected, result

b26cc75f5002212956fc783c3d72f49242e7c95a
Tobias Eilert <github@pentokami.de>
2022-07-08 16:30:41 +0200

    Investigator characteristics
    It is already notable that Characteristics could be their own class. We
    will deal with this later.

--- app/models/investigator.rb
+++ app/models/investigator.rb
@@ -2,4 +2,20 @@ class Investigator < ApplicationRecord
   def max_hp
     (siz + con) / 10
   end
+
+  def characteristics
+    %i[str con dex int siz pow app edu]
+  end
+
+  def hard_value(characteristic)
+    raise "Invalid Characteristic" unless characteristics.include?(characteristic)
+
+    self.send(characteristic) / 2
+  end
+
+  def extreme_value(characteristic)
+    raise "Invalid Characteristic" unless characteristics.include?(characteristic)
+
+    self.send(characteristic) / 5
+  end
 end
--- test/models/investigator_test.rb
+++ test/models/investigator_test.rb
@@ -14,4 +14,30 @@ class InvestigatorTest < ActiveSupport::TestCase
     # Before rounding, Clara has 8.6 HP max:
     assert_equal(8, @clara.max_hp)
   end
+
+  test "returns characteristics" do
+    characteristics = @hoff.characteristics
+
+    expected_characteristics = %i[
+      str con dex int
+      siz pow app edu
+    ]
+
+    assert_equal expected_characteristics, characteristics
+  end
+
+  test "returns the hard value for a characteristic" do
+    assert_equal 30, @hoff.hard_value(:str)
+    assert_equal 19, @clara.hard_value(:con)
+  end
+
+  test "returns the extreme value for a characteristic" do
+    assert_equal 12, @hoff.extreme_value(:str)
+    assert_equal 7, @clara.extreme_value(:con)
+  end
+
+  test "raises when passing a non-characteristic" do
+    assert_raises { @hoff.hard_value(:no_characteristic) }
+    assert_raises { @hoff.extreme_value(:no_characteristic) }
+  end
 end
